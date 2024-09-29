7db49ffbd0a4de9007448cb0c1a2fcc1eb2eb4da
Wolfgang Reithmeier <wolfgang.reithmeier@check24.de>
2024-09-29 21:51:50 +0200

    Base: upload (mux router + basic checking)

--- main.go
+++ main.go
@@ -3,25 +3,43 @@ package main
 import (
 	"OpenSPMRegistry/controller"
 	"flag"
+	"github.com/gorilla/mux"
 	"log"
+	"log/slog"
 	"net/http"
 )
 
 var (
-	tlsFlag bool
+	tlsFlag     bool
+	verboseFlag bool
 )
 
 func main() {
 	flag.BoolVar(&tlsFlag, "tls", false, "enable tls enabled")
+	flag.BoolVar(&verboseFlag, "v", false, "show more information")
 	flag.Parse()
 
-	http.HandleFunc("/", controller.MainAction)
+	if verboseFlag {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	} else {
+		slog.SetLogLoggerLevel(slog.LevelInfo)
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", controller.MainAction)
+	router.HandleFunc("/{scope}/{package}/{version}", controller.PublishAction).Methods("PUT")
+
+	srv := &http.Server{
+		Addr:    ":1234",
+		Handler: http.Handler(router),
+	}
 
 	if tlsFlag {
-		log.Println("Starting HTTPS server on :1234")
-		log.Fatal(http.ListenAndServeTLS(":1234", "server.crt", "server.key", nil))
+		slog.Info("Starting HTTPS server on", srv.Addr)
+		log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
 	} else {
-		log.Println("Starting HTTP server on :1234")
-		log.Fatal(http.ListenAndServe(":1234", nil))
+		slog.Info("Starting HTTP server on", srv.Addr)
+		log.Fatal(srv.ListenAndServe())
 	}
 }
