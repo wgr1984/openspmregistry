c04538a1ab724a6a935aa5453ebde6aef19fb476
Wolfgang Reithmeier <wolfgang.reithmeier@check24.de>
2024-09-29 19:14:56 +0200

    Base: include tls functionality (localhost mainly)

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"OpenSPMRegistry/controller"
+	"flag"
 	"log"
 	"net/http"
 )
 
+var (
+	tlsFlag bool
+)
+
 func main() {
+	flag.BoolVar(&tlsFlag, "tls", false, "enable tls enabled")
+	flag.Parse()
+
 	http.HandleFunc("/", controller.MainAction)
 
-	log.Fatal(http.ListenAndServe(":1234", nil))
+	if tlsFlag {
+		log.Println("Starting HTTPS server on :1234")
+		log.Fatal(http.ListenAndServeTLS(":1234", "server.crt", "server.key", nil))
+	} else {
+		log.Println("Starting HTTP server on :1234")
+		log.Fatal(http.ListenAndServe(":1234", nil))
+	}
 }
