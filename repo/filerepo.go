f3e9de39e5c07c3f389d35fa188302d277e8b92f
Wolfgang Reithmeier <w.reithmeier@gmail.com>
2024-10-06 00:25:26 +0200

    Base: upload (extrack Package swifts)

--- repo/filerepo.go
+++ repo/filerepo.go
@@ -26,7 +26,7 @@ func (f *FileRepo) Exists(element *models.Element) bool {
 }
 
 func (f *FileRepo) Write(element *models.Element, reader io.Reader) error {
-	pathFolder := filepath.Join(f.path, element.Scope, element.Name)
+	pathFolder := filepath.Join(f.path, element.Scope, element.Name, element.Version)
 	if _, err := os.Stat(pathFolder); errors.Is(err, os.ErrNotExist) {
 		err := os.MkdirAll(pathFolder, os.ModePerm)
 		if err != nil {
@@ -38,16 +38,11 @@ func (f *FileRepo) Write(element *models.Element, reader io.Reader) error {
 	pathFile := filepath.Join(pathFolder, element.FileName())
 	file, err := os.Create(pathFile)
 	if err != nil {
+		if fileCloseErr := closeFile(pathFile, file); fileCloseErr != nil {
+			return fileCloseErr
+		}
 		return err
 	}
-
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			slog.Error(err.Error())
-		}
-		slog.Info("Filerepo closed", "filename", element.FileName())
-	}(file)
 
 	b := make([]byte, 512)
 	for {
@@ -55,15 +50,74 @@ func (f *FileRepo) Write(element *models.Element, reader io.Reader) error {
 		slog.Debug("Filerepo read", "count", count)
 		wrote, writeErr := file.Write(b[:count])
 		if writeErr != nil {
+			if fileCloseErr := closeFile(pathFile, file); fileCloseErr != nil {
+				return fileCloseErr
+			}
 			return writeErr
 		}
 		slog.Debug("Filerepo wrote", "count", wrote)
 		if err == io.EOF {
-			slog.Debug("filerepo EOF", "filename", element.FileName())
+			slog.Debug("filerepo EOF", "filename", pathFile)
 			break
 		} else if err != nil {
+			if fileCloseErr := closeFile(pathFile, file); fileCloseErr != nil {
+				return fileCloseErr
+			}
 			return err
 		}
 	}
+
+	if fileCloseErr := closeFile(pathFile, file); fileCloseErr != nil {
+		return fileCloseErr
+	}
+
+	return ExtractPackageSwiftFiles(element, pathFile, writePackageSwiftFiles(pathFolder))
+}
+
+func writePackageSwiftFiles(pathFolder string) func(name string, r io.ReadCloser) error {
+	return func(name string, r io.ReadCloser) error {
+		// write to file
+		pathFile := filepath.Join(pathFolder, name)
+		file, err := os.Create(pathFile)
+		if err != nil {
+			if fileCloseErr := closeFile(pathFile, file); fileCloseErr != nil {
+				return fileCloseErr
+			}
+			return err
+		}
+
+		b := make([]byte, 512)
+		for {
+			count, err := r.Read(b)
+			slog.Debug("Filerepo read", "filename", pathFile)
+			wrote, writeErr := file.Write(b[:count])
+			if writeErr != nil {
+				if fileCloseErr := closeFile(pathFile, file); fileCloseErr != nil {
+					return fileCloseErr
+				}
+				return writeErr
+			}
+			slog.Debug("Filerepo wrote", "filename", pathFile, "count", wrote)
+			if err == io.EOF {
+				slog.Debug("filerepo EOF", "filename", pathFile)
+				break
+			} else if err != nil {
+				if fileCloseErr := closeFile(pathFile, file); fileCloseErr != nil {
+					return fileCloseErr
+				}
+				return err
+			}
+		}
+		return closeFile(pathFile, file)
+	}
+}
+
+func closeFile(name string, file *os.File) error {
+	if file != nil {
+		if err := file.Close(); err != nil {
+			return err
+		}
+	}
+	slog.Info("Filerepo closed", "filename", name)
 	return nil
 }
---
+++ repo/utils.go
@@ -0,0 +1,66 @@
+package repo
+
+import (
+	"OpenSPMRegistry/models"
+	"archive/zip"
+	"io"
+	"path/filepath"
+	"strings"
+)
+
+func ExtractPackageSwiftFiles(element *models.Element, fileLocation string, packageSwiftReader func(name string, r io.ReadCloser) error) error {
+	// extract Package Swifts
+	if element.MimeType == "application/zip" {
+		r, err := zip.OpenReader(fileLocation)
+		if err != nil {
+			if r != nil {
+				if e := r.Close(); e != nil {
+					return e
+				}
+			}
+			return err
+		}
+
+		for _, file := range r.File {
+
+			filename := file.FileInfo().Name()
+			ext := filepath.Ext(filename)
+
+			if strings.HasPrefix(filename, "Package") && ext == ".swift" {
+				readerCloser, err := file.Open()
+				if err != nil {
+					if e := ensureFileReaderClosed(readerCloser, r); e != nil {
+						return e
+					}
+					return err
+				}
+
+				if errReader := packageSwiftReader(filename, readerCloser); errReader != nil {
+					if e := ensureFileReaderClosed(readerCloser, r); e != nil {
+						return e
+					}
+					return errReader
+				}
+
+				if e := ensureFileReaderClosed(readerCloser, r); e != nil {
+					return e
+				}
+			}
+		}
+
+		if e := r.Close(); e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+func ensureFileReaderClosed(readerCloser io.ReadCloser, r *zip.ReadCloser) error {
+	if e := readerCloser.Close(); e != nil {
+		if e2 := r.Close(); e2 != nil {
+			return e2
+		}
+		return e
+	}
+	return nil
+}
